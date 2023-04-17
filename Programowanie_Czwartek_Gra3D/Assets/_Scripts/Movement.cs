2d838daa795de06d5bba39c7236c52fbebab4d0e
DarkTuck <98091387+DarkTuck@users.noreply.github.com>
2023-04-17 17:52:00 +0200

    Unity >:-[ Znowu coś nie działa bo unity nie umie we własne klasy mam dość wychodze jeszcze jebanie się z gitem przez godzinę FUCK OFF UNITY AND GIT

---
+++ Programowanie_Czwartek_Gra3D/Assets/_Scripts/Movement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    private Inputs input;
+    [SerializeField] float speed=1;
+    
+    private InputAction turning;
+    #region inputInit
+    private void OnEnable()
+    {
+        turning = input.Player.Move;
+        input.Player.Enable();
+    }
+    private void OnDisable() => input.Player.Disable();
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(turning.ReadValue<Vector2>().x + 0, 0, speed) * Time.deltaTime);
+    }
+}
